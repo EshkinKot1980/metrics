02687ee696377c574ab81bb4ae750341b15077e5
Aleksey Kamyrin <akamyrin@yandex.ru>
2025-04-29 06:01:49 +0300

    Iter4 Add flags

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,28 +1,56 @@
 package main
 
 import (
+	"flag"
 	"time"
 	"github.com/EshkinKot1980/metrics/internal/agent/monitor"
 	"github.com/EshkinKot1980/metrics/internal/agent/client"
 	"github.com/EshkinKot1980/metrics/internal/agent/storage"
 )
 
+type Config struct {
+	schema         string
+	address 	   string
+	URL			   string
+	PollInterval   int
+	ReportInterval int
+}
+
 func main() {
+	cfg := loadConfig()
 	s := storage.New()
-	c := client.New(s, "http://localhost:8080")
+	c := client.New(s, cfg.URL)
 	m := monitor.New(s)
 
 	go func() {
-		interval := time.Duration(2) * time.Second
+		interval := time.Duration(cfg.PollInterval) * time.Second
 		for {
 			<-time.After(interval)
 			m.Poll()
 		}
 	}()
 
-	interval := time.Duration(10) * time.Second
+	interval := time.Duration(cfg.ReportInterval) * time.Second
 	for {
 		<-time.After(interval)
 		c.Report()
+	}
+}
+
+func loadConfig() *Config {
+	schema := "http"
+	addr := flag.String("a", "localhost:8080", "address to serve in format host:port")
+	pi := flag.Int("p", 2, "poll interval in seconds")
+	ri := flag.Int("r", 10, "report interval in seconds")
+	
+
+	flag.Parse()
+
+	return &Config{
+		schema:  "http",
+		address: *addr,
+		URL: schema + "://" + *addr,
+		PollInterval: *pi,
+		ReportInterval: *ri, 
 	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
+	"flag"
 	"net/http"
+	"github.com/go-chi/chi/v5"
+	
 	"github.com/EshkinKot1980/metrics/internal/server/handlers/update"
 	"github.com/EshkinKot1980/metrics/internal/server/handlers/value"
 	"github.com/EshkinKot1980/metrics/internal/server/storage/memory"
-	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	addr := flag.String("a", "localhost:8080", "address to serve in format host:port")
+	flag.Parse()
 
-	storage := memory.New()
-	
+	storage := memory.New()	
 	router := chi.NewRouter()
 	router.Route("/update", func(r chi.Router) {
 		r.Post("/{type}/{name}/{value}", update.New(storage))
 	})
 	router.Get("/value/{type}/{name}", value.New(storage))
 
-	err := http.ListenAndServe(`:8080`, router)
+	err := http.ListenAndServe(*addr, router)
 	if err != nil {
 		panic(err)
 	}
 }
+
+
